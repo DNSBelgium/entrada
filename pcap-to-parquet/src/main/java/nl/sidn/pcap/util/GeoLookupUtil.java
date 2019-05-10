2e8e6a699f68772e86129ac40fedcce0f3cff201
Quentin Loos <quentin.loos@dnsbelgium.be>
2019-05-10 18:51:01 +0200

    Make GeoLookupUtil singleton

--- pcap-to-parquet/src/main/java/nl/sidn/pcap/util/GeoLookupUtil.java
+++ pcap-to-parquet/src/main/java/nl/sidn/pcap/util/GeoLookupUtil.java
@@ -2,17 +2,17 @@
  * ENTRADA, a big data platform for network data analytics
  *
  * Copyright (C) 2016 SIDN [https://www.sidn.nl]
- * 
+ *
  * This file is part of ENTRADA.
- * 
+ *
  * ENTRADA is free software: you can redistribute it and/or modify it under the terms of the GNU
  * General Public License as published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * ENTRADA is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
  * the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
  * Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along with ENTRADA. If not, see
  * [<http://www.gnu.org/licenses/].
  *
@@ -32,9 +32,15 @@ import java.io.IOException;
 import java.net.InetAddress;
 
 /**
- * Utility class to lookup IP adress information such as country and asn. Uses the maxmind database
+ * Utility class to lookup IP address information such as country and asn. Uses the Maxmind database
  */
 public class GeoLookupUtil {
+
+  private static GeoLookupUtil INSTANCE = new GeoLookupUtil();
+
+  public static GeoLookupUtil getInstance() {
+    return INSTANCE;
+  }
 
   private static final Logger LOGGER = LoggerFactory.getLogger(GeoLookupUtil.class);
 
